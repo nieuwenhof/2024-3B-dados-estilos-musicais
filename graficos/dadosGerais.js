34b92f98c0d8f88253e26b96bb9ffa5f675f6a28
Lauanny Nieuwenhoff Oliveira <126528019+nieuwenhof@users.noreply.github.com>
2024-11-21 08:52:46 -0300

    Delete graficos directory

--- graficos/dadosGerais.js
+++
@@ -1,9 +0,0 @@
-const url = 'https://github.com/nieuwenhof/Api-2024-lal-/blob/main/Estilos_Musicais.json'
-
-async function vizualizainfo(){
-    const res = await fetch(url)
-    const dados = await res.json()
-
-    const Estilos_Musicais = Object.keys(dados)
-    const Estilos_MaisVotado =  [0]
-}
